97df23de7883f68b8613f2e705d33c392dfb5a9a
Chika <chika.hamayama@gmail.com>
2018-10-23 20:22:13 +0900

    delete devise_trackable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  # :trackableを削除することにより、ログインが可能になる。削除しなければNoMethodError in Devise::SessionsController#createというエラーになる。
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :validatable
   validates :name, presence: true, length: { maximum: 50 }
 end
