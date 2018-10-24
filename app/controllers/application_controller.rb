11dc3cb6066541ec9756edb722a48c15b0ea5ad8
Chika <chika.hamayama@gmail.com>
2018-10-24 14:49:12 +0900

    profilepage-edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    #nameカラム、websiteカラム、bioカラムのアップデートを許可する
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :website, :bio])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,23 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-
   # :trackableを削除することにより、ログインが可能になる。削除しなければNoMethodError in Devise::SessionsController#createというエラーになる。
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   validates :name, presence: true, length: { maximum: 50 }
+
+  def update_without_current_password(params, *options)
+    #パスワードを入力しなくてもプロフィール情報を編集できるようにする
+    params.delete(:current_password)
+
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update_attributes(params, *options)
+    clean_up_passwords
+    result
+  end
 end
