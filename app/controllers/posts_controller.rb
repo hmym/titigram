3e94e6b0c255052ba28437c37986c025df12f415
Chika <chika.hamayama@gmail.com>
2018-10-27 12:39:41 +0900

    layout change post_list

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
 # ログインしているユーザーのみアクセスできるようにする
   before_action :authenticate_user!
   def index
-    @posts = Post.limit(10).includes(:photos)
+    @posts = Post.limit(10).includes(:photos, :user).order('created_at DESC')
   end
 
   def new
